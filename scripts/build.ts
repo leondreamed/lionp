2c8a5f91f0bef8163b7388d884710cad151240dc
Leon Si <contact@leonzalion.com>
2022-01-18 19:47:27 -0500

    refactor: use lion-system

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,6 @@
-import * as path from 'node:path';
-import * as fs from 'node:fs';
+import { copyPackageFiles, rmDist } from 'lion-system';
 import { execaCommandSync as exec } from 'execa';
 
-const rootPath = new URL('..', import.meta.url).pathname;
-
-fs.rmSync(path.join(rootPath, 'dist'), { recursive: true, force: true });
+rmDist();
 exec('tsc');
-for (const file of ['package.json', 'README.md'])
-	fs.copyFileSync(file, path.join('dist', file));
+await copyPackageFiles();
