46a3ee9002928f2540606d5d1c51a44f84489e62
Leon Si <contact@leonzalion.com>
2022-05-14 08:17:32 -0400

    test: refactor

--- test/suites/npmignore.test.ts
+++ test/suites/npmignore.test.ts
@@ -1,6 +1,5 @@
 import path from 'path';
-import test from 'ava';
-import proxyquire from 'proxyquire';
+import { test, expect } from 'vitest';
 
 const newFiles = [
 	'source/ignore.txt',
@@ -8,167 +7,230 @@ const newFiles = [
 	'.hg',
 	'test/file.txt',
 	'readme.md',
-	'README.txt'
+	'README.txt',
 ];
 
 test('ignored files using file-attribute in package.json with one file', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['pay_attention.txt']}, newFiles), ['source/ignore.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ files: ['pay_attention.txt'] },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt']);
 });
 
 test('ignored file using file-attribute in package.json with directory', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['source']}, newFiles), []);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ files: ['source'] },
+			newFiles
+		)
+	).toEqual([]);
 });
 
 test('ignored test files using files attribute and directory structure in package.json', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['source'], directories: {test: 'test-tap'}}, newFiles), ['test/file.txt']);
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['source'], directories: {test: ['test-tap']}}, newFiles), ['test/file.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ files: ['source'], directories: { test: 'test-tap' } },
+			newFiles
+		)
+	).toEqual(['test/file.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ files: ['source'], directories: { test: ['test-tap'] } },
+			newFiles
+		)
+	).toEqual(['test/file.txt']);
 });
 
 test('ignored files using .npmignore', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({name: 'npmignore'}, newFiles), ['source/ignore.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ name: 'npmignore' },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt']);
 });
 
 test('ignored test files using files attribute and .npmignore', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({directories: {test: 'test-tap'}}, newFiles), ['source/ignore.txt', 'test/file.txt']);
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({directories: {test: ['test-tap']}}, newFiles), ['source/ignore.txt', 'test/file.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ directories: { test: 'test-tap' } },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt', 'test/file.txt']);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ directories: { test: ['test-tap'] } },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt', 'test/file.txt']);
 });
 
 test('ignored files - dot files using files attribute', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({files: ['source']}, ['test/.dotfile']), []);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles({ files: ['source'] }, [
+			'test/.dotfile',
+		])
+	).toEqual([]);
 });
 
 test('ignored files - dot files using .npmignore', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({}, ['test/.dot']), []);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles({}, ['test/.dot'])
+	).toEqual([]);
 });
 
 test('ignored files - ignore strategy is not used', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures'),
+		},
 	});
-	t.deepEqual(await testedModule.getNewAndUnpublishedFiles({name: 'no ignore strategy'}, newFiles), []);
+	expect(
+		await testedModule.getNewAndUnpublishedFiles(
+			{ name: 'no ignore strategy' },
+			newFiles
+		)
+	).toEqual([]);
 });
 
 test('first time published files using file-attribute in package.json with one file', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({files: ['pay_attention.txt']}, newFiles), ['source/pay_attention.txt']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles(
+			{ files: ['pay_attention.txt'] },
+			newFiles
+		)
+	).toEqual(['source/pay_attention.txt']);
 });
 
 test('first time published files using file-attribute in package.json with directory', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({files: ['source']}, newFiles), ['source/ignore.txt', 'source/pay_attention.txt']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles(
+			{ files: ['source'] },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt', 'source/pay_attention.txt']);
 });
 
 test('first time published files using .npmignore', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({name: 'npmignore'}, newFiles), ['source/pay_attention.txt']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles(
+			{ name: 'npmignore' },
+			newFiles
+		)
+	).toEqual(['source/pay_attention.txt']);
 });
 
 test('first time published dot files using files attribute', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({files: ['source']}, ['source/.dotfile']), ['source/.dotfile']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles({ files: ['source'] }, [
+			'source/.dotfile',
+		])
+	).toEqual(['source/.dotfile']);
 });
 
 test('first time published dot files using .npmignore', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({}, ['source/.dotfile']), ['source/.dotfile']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles({}, ['source/.dotfile'])
+	).toEqual(['source/.dotfile']);
 });
 
 test('first time published files - ignore strategy is not used', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({name: 'no ignore strategy'}, newFiles), ['source/ignore.txt', 'source/pay_attention.txt', 'test/file.txt']);
+	expect(
+		await testedModule.getFirstTimePublishedFiles(
+			{ name: 'no ignore strategy' },
+			newFiles
+		)
+	).toEqual(['source/ignore.txt', 'source/pay_attention.txt', 'test/file.txt']);
 });
 
 test('first time published files - empty files property', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'package')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'package'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({files: []}, newFiles), []);
+	expect(
+		await testedModule.getFirstTimePublishedFiles({ files: [] }, newFiles)
+	).toEqual([]);
 });
 
 test('first time published files - .npmignore excludes everything', async () => {
 	const testedModule = proxyquire('../source/npm/util', {
-		'pkg-dir':
-			{
-				sync: () => path.resolve('test', 'fixtures', 'npmignore')
-			}
+		'pkg-dir': {
+			sync: () => path.resolve('test', 'fixtures', 'npmignore'),
+		},
 	});
-	t.deepEqual(await testedModule.getFirstTimePublishedFiles({name: 'excluded everything'}, ['source/ignore.txt']), []);
+	expect(
+		await testedModule.getFirstTimePublishedFiles(
+			{ name: 'excluded everything' },
+			['source/ignore.txt']
+		)
+	).toEqual([]);
 });
